59c219ab1a236949304df848a2e4ae871831ff11
MalikKtK <mali0237@stud.kea.dk>
2023-02-17 08:54:39 +0100

    fa

--- js/operator.js
+++ js/operator.js
@@ -3,3 +3,19 @@ console.log(a++)
 console.log(++a)
 let b = 2
 console.log(b**3+2)
+let c = -1
+console.log(25-10-5)
+
+let dyt = 1 + 2 + 3
+
+let x,y
+x = y = 25 - 10 -5
+console.log(x,y)
+
+let minus = -1
+let plus = 5
+console.log(minus)
+console.log(typeof + "5")
+let asci = + "b"
+console.log(asci)
+console.log(typeof 5 + "5")
